7fb1d7074d5b093b375fd640faecb1b27b599ba0
Scott Spence <scottspence10@gmail.com>
2017-02-08 09:11:46 +0000

    add dotenv clean up package.json add standard as a dev dependency remove sentiment api key

--- src/bot.js
+++ src/bot.js
@@ -1,15 +1,15 @@
 /**
  * DEPENDENCIES
  */
-"use strict" // c9 use
-var twit = require('twit')
+'use strict' // c9 use
+var Twit = require('twit')
 var ura = require('unique-random-array')
 var config = require('./config')
 var sentiment = require('./helpers/sentiment')
 var db = require('./helpers/db')
 var strings = require('./helpers/strings')
 
-var Twitter = new twit(config)
+var Twitter = new Twit(config)
 
 // Frequency in minutes
 var retweetFrequency = 30
@@ -23,14 +23,14 @@ console.log('Welcome to #100DaysOfCode')
 
 // RETWEET
 // find latest tweets according to #100DaysOfCode
-var retweet = function() {
+var retweet = function () {
   var params = {
     q: queryString,
     result_type: 'recent',
     lang: 'en'
   }
   // for more parameters options, see: https://dev.twitter.com/rest/reference/get/search/tweets
-  Twitter.get('search/tweets', params, function(err, data) {
+  Twitter.get('search/tweets', params, function (err, data) {
     // if no errors
     if (!err) {
       // grab ID of tweet to retweet
@@ -38,19 +38,15 @@ var retweet = function() {
       // Tell Twitter to retweet
       Twitter.post('statuses/retweet/:id', {
         id: retweetId
-      }, function(err, response) {
+      }, function (err, response) {
         // if error while retweet
         if (err) {
           console.log('While Retweet. ERROR!...Maybe Duplicate Tweet')
-        }
-        else {
+        } else {
           console.log('Retweet. SUCCESS!')
         }
-
       })
-    }
-    // if unable to search a tweet
-    else {
+    } else {     // if unable to search a tweet
       console.log('Cannot Search Tweet. ERROR!')
     }
   })
@@ -62,7 +58,7 @@ setInterval(retweet, 1000 * 60 * retweetFrequency)
 
 // FAVORITE ==============================
 // find a random tweet using querySring and 'favorite' it
-var favoriteTweet = function() {
+var favoriteTweet = function () {
   var params = {
     q: queryString,
     result_type: 'recent',
@@ -71,22 +67,21 @@ var favoriteTweet = function() {
   // for more parameters, see: https://dev.twitter.com/rest/reference
 
   // find a tweet
-  Twitter.get('search/tweets', params, function(err, data) {
+  Twitter.get('search/tweets', params, function (err, data) {
     // find tweets randomly
     var tweet = data.statuses
-    var randomTweet = ranDom(tweet) //pick a random tweet
+    var randomTweet = ranDom(tweet) // pick a random tweet
 
-    //if random tweet is found
-    if (typeof randomTweet != 'undefined') {
+    // if random tweet is found
+    if (typeof randomTweet !== 'undefined') {
       // Tell Twitter to 'favorite' it
       Twitter.post('favorites/create', {
         id: randomTweet.id_str
-      }, function(err, response) {
+      }, function (err, response) {
         // if error while 'favorite'
         if (err) {
           console.log('Cannot Favorite. ERROR!')
-        }
-        else {
+        } else {
           console.log('Favorite Done. SUCCESS!')
         }
       })
@@ -107,7 +102,7 @@ var userStream = Twitter.stream('user')
 userStream.on('follow', followed)
 
 // ...trigger the callback
-function followed(event) {
+function followed (event) {
   console.log('Follow Event now RUNNING')
   // get USER's twitter handler (screen name)
   var name = event.source.name
@@ -117,15 +112,14 @@ function followed(event) {
 }
 
 // function definition to tweet back to USER who followed
-function tweetNow(tweetTxt) {
+function tweetNow (tweetTxt) {
   var tweet = {
     status: tweetTxt
   }
-  Twitter.post('statuses/update', tweet, function(err, data, response) {
+  Twitter.post('statuses/update', tweet, function (err, data, response) {
     if (err) {
-      console.log("Cannot Reply to Follower. ERROR!")
-    }
-    else {
+      console.log('Cannot Reply to Follower. ERROR!')
+    } else {
       console.log('Reply to follower. SUCCESS!')
     }
   })
@@ -137,14 +131,13 @@ const hashtagStream = Twitter.stream('statuses/filter', {
 })
 
 // Function that checks if day 1 or day 100
-var checkIfFirstOrLastDay = function() {
+var checkIfFirstOrLastDay = function () {
   hashtagStream.on('tweet', (tweet) => {
     if (checkIfLastDay(tweet)) {
       console.log(`Sending a congrats to @${tweet.user.screen_name}`)
       tweetNow(`WOOT! You did it @${tweet.user.screen_name}! Party Time!`)
-    }
-    else if (checkIfFirstDay(tweet)) {
-      console.log('Sending a congrats to @${tweet.user.screen_name}')
+    } else if (checkIfFirstDay(tweet)) {
+      console.log(`Sending a congrats to @${tweet.user.screen_name}`)
       tweetNow(`Congrats on your first day @${tweet.user.screen_name}! Keep it up!`)
     }
   })
@@ -154,7 +147,7 @@ setImmediate(checkIfFirstOrLastDay, 1000 * 60 * firstOrLastDayFrequency)
 
 // NOTE: String elements in firstDay & lastDay are case insensitive
 
-function checkIfFirstDay(tweet) {
+function checkIfFirstDay (tweet) {
   const firstDay = ['first day', 'day one', 'day 1/100']
   const firstdayRegex = /\bday\s?0?1\b/i
   console.log(`Checking if first day`)
@@ -165,7 +158,7 @@ function checkIfFirstDay(tweet) {
   }
 }
 
-function checkIfLastDay(tweet) {
+function checkIfLastDay (tweet) {
   const lastDay = ['#day100', 'final day', 'day 100', 'one hundred', '100/100']
   const lastdayRegex = /\bday\s?100\b/i
   console.log(`Checking if Last day`)
@@ -176,28 +169,26 @@ function checkIfLastDay(tweet) {
   }
 }
 
-function checkTweetForText(tweetText, value) {
+function checkTweetForText (tweetText, value) {
   return tweetText.toLowerCase().indexOf(value) > -1 && tweetText.toLowerCase().indexOf('100daysofcode') > -1
 }
 
-function ranDom(arr) {
+function ranDom (arr) {
   var index = Math.floor(Math.random() * arr.length)
   return arr[index]
 }
 
 // PROJECT OF THE DAY TWEET
-function tweetProjectOfTheDay() {
-
+function tweetProjectOfTheDay () {
   var projectOfTheDay = ura(strings.projectOfTheDay)
 
   var message = 'Todays #100DaysOfCode #301DaysOfCode #ProjectOfTheDay, ' + projectOfTheDay()
 
   Twitter.post('statuses/update', {
     status: message
-  }, function(err, data, response) {
+  }, function (err, data, response) {
     console.log('POST PROJECT OF THE DAY!')
   })
-
 }
 
 // // post random project of the day
@@ -210,42 +201,38 @@ const hashtagStream2 = Twitter.stream('statuses/filter', {
   track: '#100DaysOfCode'
 })
 
-var sentimentBot = function() {
+var sentimentBot = function () {
   hashtagStream2.on('tweet', (tweet) => {
     console.log(`Sentiment Bot Running`)
       //  Setup the http call
     var httpCall = sentiment.init()
 
     // Don't do anything if it's the bot tweet
-    if (tweet.user.screen_name == '_100DaysOfCode') return
+    if (tweet.user.screen_name === '_100DaysOfCode') return
 
-    httpCall.send("txt=" + tweet.text).end(function(result) {
-
+    httpCall.send('txt=' + tweet.text).end(function (result) {
       var sentim = result.body.result.sentiment
       var confidence = parseFloat(result.body.result.confidence)
 
       // if sentiment is Negative and the confidence is above 75%
-      if (sentim == 'Negative' && confidence >= 75) {
-
+      if (sentim === 'Negative' && confidence >= 75) {
         // get a random quote
         var phrase = sentiment.randomQuote()
-        var screen_name = tweet.user.screen_name
+        var screenName = tweet.user.screen_name
 
         // Check key isn't in db already, key being the screen_name
-        db.get(screen_name, function(err, value) {
-
-          if (typeof(value) !== 'undefined') {
-            console.log('ALREADY IN DB USER ', screen_name)
-          }
-          else {
-            // Put a user name and that they have been encouraged 
-            db.put(screen_name, 'encourage', function(err) {
+        db.get(screenName, function (err, value) {
+          if (typeof (value) !== 'undefined') {
+            console.log('ALREADY IN DB USER ', screenName)
+          } else {
+            // Put a user name and that they have been encouraged
+            db.put(screenName, 'encourage', function (err) {
               if (err) return console.log('Ooops!', err) // some kind of I/O error
 
-              console.log('LOGGED USER ', screen_name)
+              console.log('LOGGED USER ', screenName)
 
               // tweet a random encouragement phrase
-              tweetNow('@' + screen_name + ' ' + phrase)
+              tweetNow('@' + screenName + ' ' + phrase)
             })
           }
         })
@@ -256,10 +243,10 @@ var sentimentBot = function() {
 
 sentimentBot()
 
-var refreshDB = function() {
+var refreshDB = function () {
   var fs = require('fs-extra')
 
-  fs.remove('./blacklistUsersDb', function(err) {
+  fs.remove('./blacklistUsersDb', function (err) {
     if (err) return console.error(err)
 
     console.log('success!')
