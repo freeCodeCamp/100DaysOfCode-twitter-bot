0cf9d7bb31d168a8ec9d3617a1ed3b8ea9ad82b5
amandeepmittal <amandeepmittal@live.com>
2017-03-30 15:45:27 +0530

    add api keys to src/config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,15 @@
-require('dotenv').config()
-
+/*
+* TWITTER APP CONFIGURATION
+* consumer_key
+* consumer_secret
+* access_token
+* access_token_secret
+*/
 module.exports = {
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET
-}
+  consumer_key: '',
+  consumer_secret: '',
+  access_token: '',
+  access_token_secret: '',
+  sentiment_api_key: '',
+  queryString: '#100DaysOfCode OR #100daysofcode OR #301DaysOfCode'
+};
