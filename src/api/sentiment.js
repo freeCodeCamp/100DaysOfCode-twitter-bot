f8f26f5ec452ce732d488b70cbe2f3c2ec43f5da
spences10 <spences10apps@gmail.com>
2017-11-17 08:32:21 +0000

    try new sentiment detection with retext-sentiment

--- src/api/sentiment.js
+++ src/api/sentiment.js
@@ -1,10 +1,15 @@
 'use strict'
 
-const sentiment = require('../helpers/sentiment')
+// const sentiment = require('../helpers/sentiment')
 const config = require('../config')
 const twit = require('twit')
 const T = new twit(config.twitter)
 const db = require('../helpers/db')
+
+const unified = require('unified')
+const sentiment = require('retext-sentiment')
+const english = require('retext-english')
+const inspect = require('unist-util-inspect')
 
 const hashtagStream2 = T.stream('statuses/filter', {
   track: config.queryString
@@ -13,51 +18,70 @@ const hashtagStream2 = T.stream('statuses/filter', {
 const sentimentBot = () => {
   hashtagStream2.on('tweet', tweet => {
     console.log(`Sentiment Bot Running`)
+    
+    const processor = unified()
+      .use(english)
+      .use(sentiment)
 
-    //  Setup the http call
-    const httpCall = sentiment.init()
+    const tree = processor.parse(tweet.text)
 
-    // Don't do anything if it's the bot tweet
-    if (tweet.user.screen_name == '_100DaysOfCode') return
+    processor.run(tree, tweet.text)
 
-    httpCall.send('txt=' + tweet.text).end(result => {
-      // try
-      try {
-        let sentim = result.body.result.sentiment
-      } catch (err) {
-        // derp out
-        console.log('====================')
-        console.log(err)
-        console.log('====================')
-        return
-      }
-      let confidence = parseFloat(result.body.result.confidence)
+    console.log('====================')
+    console.log(tree)
+    console.log('====================')
 
-      // if sentiment is Negative and the confidence is above 75%
-      if (sentim == 'Negative' && confidence >= 75) {
-        // get a random quote
-        let phrase = sentiment.randomQuote()
-        let screen_name = tweet.user.screen_name
+    console.log(inspect(tree))
 
-        // Check key isn't in db already, key being the screen_name
-        db.get(screen_name, (err, value) => {
-          if (typeof value !== 'undefined') {
-            console.log('ALREADY IN DB USER ', screen_name)
-          } else {
-            // Put a user name and that they have been encouraged
-            db.put(screen_name, 'encourage', err => {
-              // some kind of I/O error
-              if (err) return console.log('Ooops!', err)
+    // // Setup the http call
+    // const httpCall = sentiment.init()
 
-              console.log('LOGGED USER: ', screen_name)
+    // // Don't do anything if it's the bot tweet
+    // if (tweet.user.screen_name == '_100DaysOfCode') return
 
-              // tweet a random encouragement phrase
-              tweetNow('@' + screen_name + ' ' + phrase)
-            })
-          }
-        })
-      }
-    })
+    // httpCall.send('txt=' + tweet.text).end(result => {
+    //   console.log('====================')
+    //   console.log(result.status)
+    //   console.log('====================')
+    //   console.log(result.headers)
+    //   console.log('====================')
+    //   console.log(result.body)
+    //   console.log('====================')
+    //   // try
+    //   try {
+    //     let sentim = result.body.result.sentiment
+    //     let confidence = parseFloat(result.body.result.confidence)
+    //   } catch (err) {
+    //     // derp out
+    //     console.log(err)
+    //     return
+    //   }
+
+    //   // if sentiment is Negative and the confidence is above 75%
+    //   if (sentim == 'Negative' && confidence >= 75) {
+    //     // get a random quote
+    //     let phrase = sentiment.randomQuote()
+    //     let screen_name = tweet.user.screen_name
+
+    //     // Check key isn't in db already, key being the screen_name
+    //     db.get(screen_name, (err, value) => {
+    //       if (typeof value !== 'undefined') {
+    //         console.log('ALREADY IN DB USER ', screen_name)
+    //       } else {
+    //         // Put a user name and that they have been encouraged
+    //         db.put(screen_name, 'encourage', err => {
+    //           // some kind of I/O error
+    //           if (err) return console.log('Ooops!', err)
+
+    //           console.log('LOGGED USER: ', screen_name)
+
+    //           // tweet a random encouragement phrase
+    //           tweetNow('@' + screen_name + ' ' + phrase)
+    //         })
+    //       }
+    //     })
+    //   }
+    // })
   })
 }
 
--- src/helpers/sentiment.js
+++ src/helpers/sentiment.js
@@ -15,7 +15,7 @@ const sentiment = {}
 sentiment.init = () => {
   return unirest
     .post('https://community-sentiment.p.mashape.com/text/')
-    .header('X-Mashape-Key', apiKey)
+    .header('X-Mashape-Key', `${apiKey}`)
     .header('Content-Type', 'application/x-www-form-urlencoded')
     .header('Accept', 'application/json')
 }
