e2ffcb07e35ba5cb98affc7e883d7a0032f18b96
spences10 <spences10apps@gmail.com>
2017-11-27 09:23:59 +0000

    add config tests

--- src/config.test.js
+++ src/config.test.js
@@ -1,8 +1,21 @@
-const config = require('./config').twitterKeys
+const config = require('./config')
+
+const keys =config.twitterKeys
+const param =config.twitterConfig
 
 test('returns config object', () => {
-  expect(config.access_token).not.toBe(''),
-  expect(config.access_token_secret).not.toBe(''),
-  expect(config.consumer_key).not.toBe(''),
-  expect(config.consumer_secret).not.toBe('')
+  expect(keys.access_token).not.toBe(''),
+  expect(keys.access_token_secret).not.toBe(''),
+  expect(keys.consumer_key).not.toBe(''),
+  expect(keys.consumer_secret).not.toBe('')
 })
+
+test('check TWEET_TIME_OUT_MAX', () => {
+  expect(param.tweetTimeOutMax).not.toBe(undefined)
+})
+
+test('check TWEET_TIME_OUT_MIN', () => {
+  expect(param.tweetTimeOutMin).not.toBe(undefined)
+})
+
+
